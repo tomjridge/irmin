fe26604fbf60eb01eb4457d7e071fc3f3d178fa7
Tom Ridge <tom.j.ridge@googlemail.com>
2022-04-11 17:02:55 +0100

    trigger_gc is no longer an option type; for in-mem impl, it does nothing

--- src/irmin-pack/unix/ext.ml
+++ src/irmin-pack/unix/ext.ml
@@ -354,7 +354,7 @@ module Maker (Config : Conf.S) = struct
          implement a function [: repo -> string -> unit] we need to be outside
          Pack_store_IO; at least in this file, we have access to all the implementation
          parts that we could need. *)
-      let trigger_gc' (repo:repo) commit_hash_s =
+      let trigger_gc (repo:repo) commit_hash_s =
         let io = get_pack_store_io' repo in
         let args = Pack_store_IO.Trigger_gc.{
             commit_hash_s;
@@ -378,7 +378,6 @@ module Maker (Config : Conf.S) = struct
         Pack_store.clear_all_caches ();
         ()
 
-      let trigger_gc = Some trigger_gc'
     end
 
   end
